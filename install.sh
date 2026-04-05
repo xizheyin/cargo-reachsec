fa9cee014eeb64cc2b2df2dc9378e508c67a84bb
xizheyin <xizheyin@smail.nju.edu.cn>
2026-04-05 19:41:41 +0800

    fix: use pinned nightly-2025-08-09 toolchain

--- install.sh
+++ install.sh
@@ -11,11 +11,11 @@ echo "Installing reachsec..."
 cargo install --path "$TMPDIR/cargo-reachsec" --bin reachsec
 
 echo "Installing nightly toolchain..."
-rustup toolchain install nightly
-rustup component add rustc-dev llvm-tools-preview --toolchain nightly
+rustup toolchain install nightly-2025-08-09
+rustup component add rustc-dev llvm-tools-preview --toolchain nightly-2025-08-09
 
 echo "Installing call-cg4rs..."
-cargo +nightly install --path "$TMPDIR/cargo-reachsec/callgraph4rs" --force
+cargo +nightly-2025-08-09 install --path "$TMPDIR/cargo-reachsec/callgraph4rs" --force
 
 rm -rf "$TMPDIR"
 
